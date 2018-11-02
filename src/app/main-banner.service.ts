dd18da69b4a20e64d33ac7f8c2294782668fd595
Catherine Bradley <catherinetybradley@gmail.com>
2018-11-02 10:25:37 -0700

    add commit - app is reading firebase but cannot read specific index in database

--- src/app/main-banner.service.ts
+++ src/app/main-banner.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MainBanner } from './models/main-banner.model';
+// import { MAINBANNER } from './mock-pageinfo';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
@@ -11,7 +12,8 @@ export class MainBannerService {
   constructor(private database: AngularFireDatabase) {
     //defines what we want from the database
     this.mainBanner = database.list('mainbanner');
-    console.log(this.mainBanner)
+    console.log(this.mainBanner);
+    console.log(this.mainBanner[1]);
   }
 
   getMainBannerPlan() {
@@ -22,4 +24,16 @@ export class MainBannerService {
     return this.mainBanner;
   }
 
+  //I want one function in here to
+  //recieve the correct mainbanner information
+  //to send to the main-banner.component
+  //which then has the same app route in other components
+
+  // getMainBannerAbout() {
+  //   for(var i = 0; i <= MAINBANNER.length; i++) {
+  //     if (MAINBANNER[i].pageName === this.pageName ) {
+  //       return MAINBANNER[i];
+  //     }
+  //   }
+  // }
 }
