16b8114367f96b3fcf9ccb501cc582c8cafe7af2
Catherine Bradley <catherinetybradley@gmail.com>
2018-11-09 09:59:17 -0800

    removed API file in gitignore for grading

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyBRmUWKUhHqmtSOXiHjXRL7jq_bB_zVNyg",
+  authDomain: "project-fi-eb395.firebaseapp.com",
+  databaseURL: "https://project-fi-eb395.firebaseio.com",
+  projectId: "project-fi-eb395",
+  storageBucket: "project-fi-eb395.appspot.com",
+  messagingSenderId: "118106277342"
+};
